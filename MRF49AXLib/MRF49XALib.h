62ace139280f8b318762cd3ed93e7b45347e5462
Diego Aguilera <diegoaguilera@Diegos-MacBook-Pro-2.local>
2014-07-27 10:26:39 -0400

    Initial Commit
    Nothing really here yet!

---
+++ MRF49AXLib/MRF49XALib.h
@@ -0,0 +1,74 @@
+
+
+//Registers
+#define STSREG            0x01
+#define GENCREG           0x02
+#define AFCCREG           0x03
+#define TXCREG            0x04
+#define TXBREG            0x05
+#define CFSREG            0x06
+#define RXCREG            0x07
+#define BBFCREG           0x08
+#define RXFIFOREG         0x09
+#define FIFORSTREG        0x0A
+#define SYNBREG           0x0B
+#define DRSREG            0x0C
+#define PMCREG            0x0D
+#define WTSREG            0x0E
+#define DCSREG            0x0F
+#define BCSREG            0x10
+#define PLLCREG           0x11
+
+//STSREG Bit number and length
+#define TXRXFIFO          15
+#define TXRXFIFOLEN       1
+#define POR               14
+#define PORLEN            1
+#define TXOWRXOF          13
+#define TXOWRXOFLEN       1
+#define WUTINT            12
+#define WUTINTLEN         1
+#define LCEXINT           11
+#define LCEXINTLEN        1
+#define LBTD              10
+#define LBTDLEN           1
+#define FIFOEM            9
+#define FIFOEMLEN         1
+#define ATRSSI            8
+#define ATRSSILEN         1
+#define DQDO              7
+#define DQDOLEN           1
+#define CLKRL             6
+#define CLKRLLEN          1
+#define AFCCT             5
+#define AFCCTLEN          1
+#define OFFSV             4
+#define OFFSVLEN          1
+#define OFFSB             3
+#define OFFSBLEN          4
+
+//GENCREG Bit number and length
+#define TXDEN             7
+#define TXDENLEN          1
+#define FIFOEN            6
+#define FIFOENLEN         1
+#define FBS               5
+#define FBSLEN            2
+#define LCS               3
+#define LCSLEN            4
+
+//AFCCREG Bit number and length
+
+#define 
+#define 
+#define 
+#define 
+#define 
+#define 
+#define 
+#define 
+#define 
+#define 
+#define 
+
+
